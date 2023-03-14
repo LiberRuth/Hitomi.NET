ddb25cc96e666ac8f3c616108d8006b233b5ba40
LiberRuth <124418235+LiberRuth@users.noreply.github.com>
2023-03-15 08:50:46 +0900

    수정

--- Hitomi.NET/hitomi/HitomiWebp.cs
+++ Hitomi.NET/hitomi/HitomiWebp.cs
@@ -15,6 +15,7 @@ namespace Hitomi.NET
         {
             int i = 1;
             List<Task> tasks = new List<Task>();
+            SemaphoreSlim semaphore = new SemaphoreSlim(threads);
             //List<string> downloadedFiles = new List<string>();
 
             var lists = await ImageRoute.List_Hash(number);
@@ -22,41 +23,52 @@ namespace Hitomi.NET
 
             foreach (var item in lists)
             {
-                tasks.Add(Task.Run(async () =>
+                await semaphore.WaitAsync();
+
+                var task = Task.Run(async () => 
                 {
-
-                    var Files = ImageRoute.Image_Hash(item);
-                    await ImageRoute.GG.GgJS();
-                    string urls = $"https://a.hitomi.la/webp/{await ImageRoute.GG.B()}{Files}/{item}.webp";
-                    var server_number = await ImageRoute.SubdomainFromUrl(urls);
-                    string str_server = server_number[0].ToString();
-                    urls = urls.Insert(8, str_server);
-
-                    using (HttpClient httpClient = new HttpClient())
+                    try
                     {
-                        httpClient.DefaultRequestHeaders.Add("User-Agent", UA);
-                        httpClient.DefaultRequestHeaders.Referrer = new Uri("https://hitomi.la/");
-                        HttpResponseMessage response = await httpClient.GetAsync(urls);
-                        response.EnsureSuccessStatusCode();
-                        byte[] content = await response.Content.ReadAsByteArrayAsync();
+                        var Files = ImageRoute.Image_Hash(item);
+                        await ImageRoute.GG.GgJS();
+                        string urls = $"https://a.hitomi.la/webp/{await ImageRoute.GG.B()}{Files}/{item}.webp";
+                        var server_number = await ImageRoute.SubdomainFromUrl(urls);
+                        string str_server = server_number[0].ToString();
+                        urls = urls.Insert(8, str_server);
 
-                        string folderPath = $@"C:\Users\{Environment.UserName}\Downloads\{number}";
-                        DirectoryInfo di = new DirectoryInfo(folderPath);
-                        if (di.Exists == false)
+                        using (HttpClient httpClient = new HttpClient())
                         {
-                            di.Create();
-                        }
-                        System.IO.File.WriteAllBytes($@"C:\Users\{Environment.UserName}\Downloads\{number}\{number}-{i++}.png", content);
-                    }
+                            httpClient.DefaultRequestHeaders.Add("User-Agent", UA);
+                            httpClient.DefaultRequestHeaders.Referrer = new Uri("https://hitomi.la/");
+                            HttpResponseMessage response = await httpClient.GetAsync(urls);
+                            response.EnsureSuccessStatusCode();
+                            byte[] content = await response.Content.ReadAsByteArrayAsync();
 
-                    Console.WriteLine(urls);
-                    //Thread.Sleep(500);
-                }));
+                            string folderPath = $@"C:\Users\{Environment.UserName}\Downloads\{number}";
+                            DirectoryInfo di = new DirectoryInfo(folderPath);
+                            if (di.Exists == false)
+                            {
+                                di.Create();
+                            }
+                            System.IO.File.WriteAllBytes($@"C:\Users\{Environment.UserName}\Downloads\{number}\{number}-{i++}.png", content);
+                        }
+                        Console.WriteLine(urls);
+                        //Thread.Sleep(500);
+                    }
+                    finally 
+                    { 
+                        semaphore.Release();
+                    }
+                
+                });
+
+                tasks.Add(task);
 
                 if (tasks.Count >= threads) 
                 {
                     await Task.WhenAll(tasks);
-                    tasks.Clear();
+                    tasks.RemoveAll(x => x.IsCompleted);
+                    //tasks.Clear();
                 }
             }
 
