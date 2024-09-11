d25f737f9918043c2a8c5b2da7cfdc1d8f4d0259
LiberRuth <124418235+LiberRuth@users.noreply.github.com>
2024-09-11 20:23:31 +0900

    수정

--- Hitomi.NET/hitomi/HitomiWebp.cs
+++ Hitomi.NET/hitomi/HitomiWebp.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Hitomi.NET
 {
     public class HitomiWebp
     {
         public int thread { get; set; } = 1;
+        public string? path { get; set; }
+
         ImageRoute imageRoute = new ImageRoute();
         ImageRoute.GG gG = new ImageRoute.GG();
 
-        public string? path { get; set; }
-      
         public async Task HitomiDownload(int number)
         {
-            var lists = await imageRoute.List_Hash(number);
+            var mangaList = await imageRoute.List_Hash(number);
             string UA = RandomUA.UserAgent();
 
             List<Task> tasks = new List<Task>();
             SemaphoreSlim semaphore = new SemaphoreSlim(thread);
 
-            foreach (var item in lists)
+            foreach (var item in mangaList)
             {
                 await semaphore.WaitAsync();
 
@@ -29,16 +28,16 @@ namespace Hitomi.NET
                     {
                         var Files = imageRoute.Image_Hash(item);
                         await gG.GgJS();
-                        string urls = $"https://a.hitomi.la/webp/{await gG.B()}{Files}/{item}.webp";
-                        var server_number = await imageRoute.SubdomainFromUrl(urls);
+                        string mangaUrl = $"https://a.hitomi.la/webp/{await gG.B()}{Files}/{item}.webp";
+                        var server_number = await imageRoute.SubdomainFromUrl(mangaUrl);
                         string str_server = server_number[0].ToString();
-                        urls = urls.Insert(8, str_server);
+                        mangaUrl = mangaUrl.Insert(8, str_server);
 
                         using (HttpClient httpClient = new HttpClient())
                         {
                             httpClient.DefaultRequestHeaders.Add("User-Agent", UA);
                             httpClient.DefaultRequestHeaders.Referrer = new Uri($"https://hitomi.la/reader/{number}.html");
-                            HttpResponseMessage response = await httpClient.GetAsync(urls);
+                            HttpResponseMessage response = await httpClient.GetAsync(mangaUrl);
                             response.EnsureSuccessStatusCode();
                             byte[] content = await response.Content.ReadAsByteArrayAsync();
 
@@ -48,10 +47,10 @@ namespace Hitomi.NET
                             {
                                 di.Create();
                             }
-                            File.WriteAllBytes($@"{path}/{number}/{number}_p{lists.IndexOf(item)}.png", content);
+                            File.WriteAllBytes($@"{path}/{number}/{number}_p{mangaList.IndexOf(item)}.png", content);
                         }
 
-                        Console.WriteLine(urls);
+                        Console.WriteLine(mangaUrl);
                     }
                     finally
                     {
@@ -71,5 +70,49 @@ namespace Hitomi.NET
             await Task.WhenAll(tasks);
         }
 
+        public async Task<List<string>> HitomiImageList(int number)
+        {
+            var mangaListReply = new List<string>();
+            var mangaList = await imageRoute.List_Hash(number);
+
+            List<Task> tasks = new List<Task>();
+            SemaphoreSlim semaphore = new SemaphoreSlim(thread);
+
+            foreach (var item in mangaList)
+            {
+                var Files = imageRoute.Image_Hash(item);
+                await gG.GgJS();
+                string mangaUrl = $"https://a.hitomi.la/webp/{await gG.B()}{Files}/{item}.webp";
+                string server_number = await imageRoute.SubdomainFromUrl(mangaUrl);
+                string str_server = server_number[0].ToString();
+                mangaListReply.Add(mangaUrl.Insert(8, str_server));
+
+            }
+            
+            return mangaListReply;
+        }
+
+        public async Task<List<string>> HitomiImageSingleList(int number) 
+        {
+            var mangaListReply = new List<string>();
+            var mangaList = await imageRoute.List_Hash(number);
+
+            List<Task> tasks = new List<Task>();
+            SemaphoreSlim semaphore = new SemaphoreSlim(thread);
+
+            await gG.GgJS();
+
+            foreach (var item in mangaList)
+            {
+                var Files = imageRoute.Image_Hash(item);
+                string mangaUrl = $"https://a.hitomi.la/webp/{await gG.B()}{Files}/{item}.webp";
+                string server_number = await imageRoute.SubdomainFromUrl(mangaUrl);
+                string str_server = server_number[0].ToString();
+                mangaListReply.Add(mangaUrl.Insert(8, str_server));
+
+            }
+
+            return mangaListReply;
+        }
     }
 }
